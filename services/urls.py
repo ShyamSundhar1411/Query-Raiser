f120957dbf5779e56844738f030ba6ad81209c2c
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-10-03 22:06:02 +0530

    View Query

--- services/urls.py
+++ services/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
     path('create/',views.create_query,name = "create_query"),
-    path('query/<slug:slug>/view/<int:pk>',views.QueryDetailView.as_view(),name = "query_detail_view")
+    path('query/<slug:slug>/view/<int:pk>',views.QueryDetailView.as_view(),name = "query_detail_view"),
+    path('query/<int:pk>/view/<slug:slug>/approve',views.approve_query,name = "approve_query"),
+    path('query/<int:pk>/view/<slug:slug>/reject',views.reject_query,name = "reject_query")
     
 ]
--- services/views.py
+++ services/views.py
@@ -3,7 +3,7 @@ from .models import Query
 from . forms import QueryCreateForm
 from django.contrib import messages
 from django.views import generic
-from django.shortcuts import render,redirect
+from django.shortcuts import get_object_or_404, render,redirect
 from django.http.response import Http404, HttpResponse
 
 # Create your views here.
@@ -36,3 +36,24 @@ def create_query(request):
             return render(request,"services/create_query.html",{"form":query_form,"hostride_form_errors":query_form.errors})
     else:
         return render(request,"services/create_query.html",{"form":QueryCreateForm()})
+
+def approve_query(request,pk,slug):
+    query = Query.objects.get(id = pk,slug = slug)
+    if request.method == "POST" and request.user.profile.role == "Program Representative":
+        query.status = "Approved"
+        query.save()
+        messages.success(request,"Approved Query successfully")
+        return redirect("query_detail_view",pk = pk,slug = slug)
+    else:
+        messages.error(request,"Error Processing Request")
+        return redirect("query_detail_view",pk = pk,slug = slug)
+def reject_query(request,pk,slug):
+    query = Query.objects.get(id = pk,slug = slug)
+    if request.method == "POST" and request.user.profile.role == "Program Representative":
+        query.status = "Rejected"
+        query.save()
+        messages.success(request,"Rejected Query successfully")
+        return redirect("query_detail_view",pk = pk,slug = slug)
+    else:
+        messages.error(request,"Error Processing Request")
+        return redirect("query_detail_view",pk = pk,slug = slug)
