6742dee9dc1a1cbcab563604b32a67dc9f8f20a3
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-10-03 21:43:12 +0530

    View Query

--- services/urls.py
+++ services/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('create/',views.create_query,name = "create_query"),
+    path('query/<slug:slug>/view/<int:pk>',views.QueryDetailView.as_view(),name = "query_detail_view")
     
 ]
--- services/views.py
+++ services/views.py
@@ -2,9 +2,22 @@
 from .models import Query
 from . forms import QueryCreateForm
 from django.contrib import messages
+from django.views import generic
 from django.shortcuts import render,redirect
+from django.http.response import Http404, HttpResponse
 
 # Create your views here.
+#Class Based Views
+class QueryDetailView(generic.DetailView):
+    model = Query
+    template_name = "services/ViewQuery.html"
+    fields = ['title','description','date_of_creation','user','type','department']
+    def get_object(self):
+        query = super(QueryDetailView,self).get_object()
+        if  (query.user == self.request.user) or (self.request.user.profile.role == "Program Representative"):
+            return query
+        raise Http404
+#Function Based Views
 def home(request):
     queries = Query.objects.filter(user = request.user)
     return render(request,"services/home.html",{"Queries":queries})
