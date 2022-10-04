370d1c6de5398823ddab3c5506aa774b8b1a601c
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-10-04 15:46:32 +0530

    Mailing feature Done

--- queryraiser/settings.py
+++ queryraiser/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-tr$2=3g%dk2lt3^goona^fce_@r*owgwbh3qdtree7xv&#e&%z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','queryraiservitc.azurewebsites.net'],
 
 
 # Application definition
