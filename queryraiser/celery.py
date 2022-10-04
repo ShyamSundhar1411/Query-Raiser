1f9c82bb42f13669f5d17c66b58e484c1a227555
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-10-04 11:24:17 +0530

    Mailing feature Done

--- queryraiser/celery.py
+++ queryraiser/celery.py
@@ -12,7 +12,7 @@ app = Celery('queryraiser')
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.conf.update(timezone = 'Asia/Kolkata')
-app.config_from_object('django.conf:settings')
+app.config_from_object('django.conf:settings',namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks(INSTALLED_APPS)
--- services/tasks.py
+++ services/tasks.py
@@ -5,11 +5,10 @@ from queryraiser.settings import DEFAULT_FROM_EMAIL as me
 def approval_mail(self,query):
     email = query.user.email
     subject = 'Approval Mail - {}'.format(query.title)
-    content = ''' Hello {}, Hope you are doing well. This mail is for your acknowledgment that your query {} raised on {}
-    has been approved by your respective PR and will be looked upon further.
-    Thank You for your patience.
-    Best Regards
-    PR Team
+    content = ''' Hello {}, Hope you are doing well. This mail is for your acknowledgment that your query {} raised on {}has been approved by your respective PR and will be looked upon further.
+Thank You for your patience.
+Best Regards
+PR Team
     '''.format(query.user.username,query.title,query.date_of_creation)
     message = EmailMessage(subject,content,me,[email])
     message.send()
@@ -18,11 +17,10 @@ def approval_mail(self,query):
 def rejection_mail(self,query):
     email = query.user.email
     subject = 'Approval Mail - {}'.format(query.title)
-    content = ''' Hello {}, Hope you are doing well. This mail is for your acknowledgment that your query {} raised on {}
-    has been rejected by your respective PR for certain allegations. We will contact you soon.
-    Thank You for your patience.
-    Best Regards
-    PR Team
+    content = ''' Hello {}, Hope you are doing well. This mail is for your acknowledgment that your query {} raised on {} has been rejected by your respective PR for certain allegations. We will contact you soon.
+Thank You for your patience.
+Best Regards
+PR Team
     '''.format(query.user.username,query.title,query.date_of_creation)
     message = EmailMessage(subject,content,me,[email])
     message.send()
