292921f8e8553ef6b60a8a0d5a1412506daf3f44
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2022-10-04 15:32:53 +0530

    Mailing feature Done

--- queryraiser/settings.py
+++ queryraiser/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'USER':os.getenv('DB_USER'),
         'PASSWORD':os.getenv('DB_PASSWORD'),
         'HOST':os.getenv('DB_HOST'),
-        'PORT':5432,
+        'PORT':'5432',
     }
 }
 
